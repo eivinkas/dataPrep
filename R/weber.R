e97bcdb15dd27b5cdee2f1c7ad514615d84ca9e2
eivinkas <eivind.kaspersen@ntnu.no>
2023-03-08 10:57:25 +0100

    added weber function

---
+++ R/weber.R
@@ -0,0 +1,42 @@
+weberMeasure = function(df,
+                            numerosity = "dots",
+                            answer = "answer",
+                            plot = TRUE) {
+
+  # Setup data frame
+  dat = data.frame(numerosity = df[,colnames(df) == numerosity])
+  dat$answer = df[,colnames(df) == answer]
+
+  # Setup empty output frame
+  dots = sort(unique(df[,(colnames(df) == numerosity)]))
+  output = data.frame(n = dots)
+  output$CV = 0
+  output$Weber = 0
+
+  for (i in 1:length(dots)) {
+    newDat = dat[dat$numerosity == dots[i],]
+    getSD = sd(newDat$answer)
+    m = mean(newDat$answer)
+    output$CV[i] = getSD/dots[i]
+    output$Weber[i] = getSD/m
+  }
+
+  # Plot
+  if (plot) {
+    library(ggplot2)
+    plotDat1 = output[,1:2]
+    plotDat2 = output[,c(1,3)]
+    names(plotDat1) = c("n", "cv")
+    names(plotDat2) = c("n", "cv")
+    plotDat1$method = "cv"
+    plotDat2$method = "weber"
+    plotDat = rbind(plotDat1, plotDat2)
+
+    ggplot(data = plotDat, aes(x = n, y = cv, group = method, fill = method)) +
+      geom_smooth() +
+      geom_line() +
+      theme_classic()
+  }
+
+  return(output)
+}
